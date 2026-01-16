062b673e66ab5b445fcbadc6680da02c728cb7ee
calcallsalot <calcallsalot@proton.me>
2026-01-16 02:08:31 -0500

    error code if username already in use and added trim()

--- zookeepersh-web/app/api/auth/register/route.ts
+++ zookeepersh-web/app/api/auth/register/route.ts
@@ -4,7 +4,15 @@ import clientPromise from "@/lib/mongodb";
 
 export async function POST(req: Request) {
   try {
-    const { email, password, name } = await req.json();
+    const body = await req.json();
+    const { email, password, name } = body ?? {};
+
+    if (typeof email !== "string" || typeof password !== "string" || typeof name !== "string") {
+      return NextResponse.json(
+        { error: "Email, password, and username required" },
+        { status: 400 }
+      );
+    }
 
     if (!email || !password) {
       return NextResponse.json({ error: "Email and password required" }, { status: 400 });
@@ -12,14 +20,21 @@ export async function POST(req: Request) {
     if (typeof password !== "string" || password.length < 8) {
       return NextResponse.json({ error: "Password must be at least 8 characters" }, { status: 400 });
     }
-
+    const emailNorm = email.trim().toLowerCase();
+    const nameDisplay = name.trim();
+    const nameNorm = nameDisplay.toLowerCase();
     const client = await clientPromise;
     const db = client.db(); // uses DB name from MONGODB_URI
     const users = db.collection("users");
 
-    const existing = await users.findOne({ email: email.toLowerCase() });
-    if (existing) {
-      return NextResponse.json({ error: "Ema1il already in use" }, { status: 409 });
+    const existing_email = await users.findOne({ email: emailNorm });
+    const existing_username = await users.findOne({ name: nameNorm });
+    if (existing_username) {
+      return NextResponse.json({ error: "Username already in use" }, { status: 409 });
+    }
+
+    if (existing_email) {
+      return NextResponse.json({ error: "Email already in use" }, { status: 409 });
     }
 
     const passwordHash = await bcrypt.hash(password, 12);
@@ -33,7 +48,10 @@ export async function POST(req: Request) {
     });
 
     return NextResponse.json({ ok: true, userId: result.insertedId.toString() });
-  } catch {
+  } catch(err: any) {
+    if (err?.code === 11000) { // mongodb duplication error theres 110001 too
+      return NextResponse.json({ error: "Email or username already in use" }, { status: 409 });
+    }
     return NextResponse.json({ error: "Server error" }, { status: 500 });
   }
 }
